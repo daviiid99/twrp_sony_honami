87c5586f08d32cb97fb9e78d2f95b807c6741940
David <davidmolinamorales1999@gmail.com>
2021-08-07 16:04:47 +0200

    Update and rename twrp_honami.mk to omni_honami.mk

--- omni_honami.mk
+++ omni_honami.mk
@@ -7,11 +7,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/sony/honami/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := honami
-PRODUCT_NAME := twrp_honami
+PRODUCT_NAME := omni_honami
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia Z1
 PRODUCT_MANUFACTURER := sony
